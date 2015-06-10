930c13a49b639b0679cf02c9498de2b99e6225b4
Stefan <dev@recursivebytes.com>
2015-06-10 21:41:50 +0200

    Initial Commit

---
+++ ipnfo/LongToIPConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ipnfo
+{
+    public class LongToIPConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                long l = (long)value;
+                return l.ToIP().ToString();
+            }
+            catch
+            {
+                return Binding.DoNothing;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string str = (string)value;
+            Match m = Regex.Match(str, @"(\d{1,3})(\.(\d{1,3}))?(\.(\d{1,3}))?(\.(\d{1,3}))?", RegexOptions.Compiled);
+            if (m.Success)
+            {
+                try
+                {
+                    IPAddress ad = new IPAddress(new byte[] { (byte)int.Parse(m.Groups[1].Value), (byte)int.Parse(m.Groups[3].Value), (byte)int.Parse(m.Groups[5].Value), (byte)int.Parse(m.Groups[7].Value) });
+                    return ad.ToLong();
+                }
+                catch
+                {
+                    return Binding.DoNothing;
+                }
+            }
+            else
+                return Binding.DoNothing;
+        }
+    }
+}
---
+++ ipnfo/StatusToTextConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ipnfo
+{
+    public class StatusToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            HostStatus h = (HostStatus)value;
+            switch (h)
+            {
+                case HostStatus.Online:
+                    return "erreichbar";
+                case HostStatus.Offline:
+                    return "nicht erreichbar";                
+                default:
+                    return "unbekannt";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
